3b17a4d73be4ea61666889af4814f33fec7c3cbf
travis skyles <travis.skyles@gmail.com>
2020-01-07 15:26:32 -0800

    implemented car and motorcyle using a Class

--- classes/index.js
+++ classes/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const VehicleConstructor = require('./vehicle-constructor.js');
+const VehicleClass = require('./vehicle-class');
 
 // Implement a car and motorcycle using a Constructor
 const mazda = new VehicleConstructor.Car('Mazda 3');
@@ -10,5 +11,8 @@ const harley = new VehicleConstructor.Motorcycle('Harley');
 console.log(harley.name, harley.wheelie(), harley.stop());
 
 // Implement a car and motorcycle using a Class
+const subaru = new VehicleClass.Car('Subaru Impreza');
+console.log(subaru.name, subaru.drive(), subaru.stop());
 
-
+const suzuki = new VehicleClass.Motorcycle('Suzuki');
+console.log(suzuki.name, suzuki.wheelie(), suzuki.stop());
